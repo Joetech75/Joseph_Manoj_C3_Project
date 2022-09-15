c494e3df076dbf773cded00575bf273eb9e7080a
Joe M <32224632+Joetech75@users.noreply.github.com>
2022-09-15 20:44:15 +0800

    Add files via upload
    Part 3: Solution

---
+++ main/java/RestaurantService.java
@@ -0,0 +1,36 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        //Check if restaurant exists or not, if yes then return restaurant object otherwise null.
+        Restaurant restaurant= this.getRestaurants().stream()
+                .filter(_restaurant -> restaurantName.equals(_restaurant.getName()))
+                .findAny()
+                .orElse(null);
+        if(restaurant == null){
+            throw new restaurantNotFoundException(restaurantName);
+        }
+        return restaurant;
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
